cfef86f6d6bebace1e21925482c53dddf69a32a9
Chris Weed <chris@cjweed.com>
2024-07-03 13:22:28 -0500

    Blocked due to inability to determine who the current controller is

--- multiplayer.ts
+++ multiplayer.ts
@@ -5,21 +5,45 @@ namespace MultiplayerState {
 namespace Multiplayer {
     let connectedPlayers: Array<{
         connected: boolean
-        player: mp.Player
+        controller?: controller.Controller
+        // player?: mp.Player
+        player: SubsPlayer
+        boats?: Array<{ x: number, y: number }>
+        subs?: Array<{ x: number, y: number }>
     }> = []
+
+    function getControllerFromIndex(playerIndex: number) {
+        if (playerIndex === 0) return controller.player1
+        if (playerIndex === 1) return controller.player2
+
+        return null
+    }
 
     function onPlayerConnected(player: mp.Player) {
         console.log('Another player connected! ' + player.index)
         
         // Check to see if this player index is already initialized
         if (!connectedPlayers[player.index]) {
+            // Place their sprite
+            const sprite = sprites.create(assets.image`boat_dl`)
+            sprite.x = (30 * player.index) + 40
+
+            // Assign controls to that sprite
+            const controller = getControllerFromIndex(player.index)
+            // controller.onButtonEvent(ControllerButton.Left, () => )
+
+            // Create their Player
+            const plr = new SubsPlayer(controller, sprite)
+
             connectedPlayers[player.index] = {
                 connected: true,
-                player
+                controller: getControllerFromIndex(player.index),
+                player: plr,
+                boats: [sprite]
             }
             // mp.setPlayerSprite(player, sprite)
         }
-        console.log('Players: ' + JSON.stringify(connectedPlayers))
+        console.log('Players: ')
     }
 
     function onPlayerDisconnect(player: mp.Player) {
--- player.ts
+++ player.ts
@@ -1,33 +1,45 @@
-namespace Player {
-    let mySprite: Sprite
-    export let myName: number
-    let myBoats: Array<Boat> = []
+class SubsPlayer {
+    mySprite: Sprite
 
-    function move() {
-        console.log('Moving! ' + myName)
-        myBoats[0].move(10, 5)
+    constructor(ctrl: controller.Controller, sprite: Sprite) {
+        this.mySprite = sprite
+        ctrl.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, () => this.move())
     }
 
-    function perform() {
-        // mp.setPlayerState(mp.getPlayerByIndex(0), 0, 1)
-        mp.getPlayerProperty(mp.getPlayerByIndex(0), mp.PlayerProperty.Index)
-        myBoats[0].performAction()
+    move() {
+        this.mySprite.setVelocity(this.mySprite.vx + 2, 0)
     }
-
-    function createBoat() {
-        myBoats.push(new Boat(60, 60))
-    }
-
-    export function init() {
-        myName = Math.randomRange(0, 20)
-        console.log('Init player ' + myName)
-
-        myBoats[0] = new Boat(30, 30)
-
-        controller.up.onEvent(ControllerButtonEvent.Pressed, move)
-        controller.A.onEvent(ControllerButtonEvent.Pressed, perform)
-        controller.B.onEvent(ControllerButtonEvent.Pressed, createBoat)
-    }
-
-    export function onPlanningComplete() {}
 }
+// namespace Player {
+//     let mySprite: Sprite
+//     export let myName: number
+//     let myBoats: Array<Boat> = []
+
+//     function move() {
+//         console.log('Moving! ' + myName)
+//         myBoats[0].move(10, 5)
+//     }
+
+//     function perform() {
+//         // mp.setPlayerState(mp.getPlayerByIndex(0), 0, 1)
+//         mp.getPlayerProperty(mp.getPlayerByIndex(0), mp.PlayerProperty.Index)
+//         myBoats[0].performAction()
+//     }
+
+//     function createBoat() {
+//         myBoats.push(new Boat(60, 60))
+//     }
+
+//     export function init() {
+//         myName = Math.randomRange(0, 20)
+//         console.log('Init player ' + myName)
+
+//         myBoats[0] = new Boat(30, 30)
+
+//         controller.up.onEvent(ControllerButtonEvent.Pressed, move)
+//         controller.A.onEvent(ControllerButtonEvent.Pressed, perform)
+//         controller.B.onEvent(ControllerButtonEvent.Pressed, createBoat)
+//     }
+
+//     export function onPlanningComplete() {}
+// }
